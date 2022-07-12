c547fe9523b16d8209ff9f046d8a764b7ed1aaf5
Denis <kanukov.denis@gmail.com>
2022-07-12 16:22:27 +0300

    add less modules and jest

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,11 +1,12 @@
-import {hot} from 'react-hot-loader/root'
-import * as React from 'react'
+import {hot} from 'react-hot-loader/root';
+import * as React from 'react';
+import styles from './header.less';
 
 function HeaderComponent () {
   return(
     <header>
-      <h1>
-        Reddit for our own 123
+      <h1 className={styles.example}>
+        Hello React
       </h1>
     </header>
   )
