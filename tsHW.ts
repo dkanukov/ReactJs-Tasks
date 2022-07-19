3a22fadfe9380bbec7c3017c76d73ee9d058088f
Denis <kanukov.denis@gmail.com>
2022-07-19 18:17:26 +0300

    task 4

--- tsHW.ts
+++ tsHW.ts
@@ -19,9 +19,33 @@ interface MyArray<T> {
     [N: number]: T;
 
     reduce<U>(callback: (previousValue: U, currentValue: T, currentIndex: number, array: MyArray<T>) => U, initialValue: U): MyArray<T>;
-
-    reduce2(callback: (previousValue: T, currentValue: T, currentIndex: number, array: MyArray<T>) => T, initialValue: T): MyArray<T>;
+    // reduce2(callback: (previousValue: T, currentValue: T, currentIndex: number, array: MyArray<T>) => T, initialValue: T): MyArray<T>;
 }
 const myArray: MyArray<number> = [1, 2, 3, 4, 5];
 let a = myArray.reduce((a, b) => a + b, 0);
-let b = myArray.reduce2((a, b) => a + b, 0);
+
+//task4
+
+interface IHomeTask {
+    data: string;
+    numericData: number;
+    date: Date;
+    externalData: {
+        basis: number;
+        value: string;
+    }
+}
+
+type MyPartial<T> = {
+    [N in keyof T]?: T[N] extends object ? MyPartial<T[N]> : T[N];
+}
+
+const homeTask: MyPartial<IHomeTask> = {
+    externalData: {
+        value: 'win',
+    },
+}
+
+
+
+
