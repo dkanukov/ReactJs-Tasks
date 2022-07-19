a3321ac03117d15186a26d0112f1358787dbc752
Denis <kanukov.denis@gmail.com>
2022-07-19 13:36:46 +0300

    question about task3

--- tsHW.ts
+++ tsHW.ts
@@ -1,6 +1,27 @@
 //task1
-function concat(str1: string, str2: string): string {
-    return str1 + str2;
+type ConcatFn = (a: string, b: string) => string;
+const concat: ConcatFn = (a, b) => a + b;
+
+//task2
+interface Task {
+    howIDoIt: string;
+    simeArray: Array<string | number>;
+    withData: Array<object>
+}
+const MyHometask:Task = {
+    howIDoIt: "I Do It Wel",
+    simeArray: ["string one", "string two", 42],
+    withData: [{ howIDoIt: "I Do It Wel", simeArray: ["string one", 23] }],
 }
 
-console.log(concat("123", "asd"));
+//task3
+interface MyArray<T> {
+    [N: number]: T;
+
+    reduce<U>(callback: (previousValue: U, currentValue: T, currentIndex: number, array: MyArray<T>) => U, initialValue: U): MyArray<T>;
+
+    reduce2(callback: (previousValue: T, currentValue: T, currentIndex: number, array: MyArray<T>) => T, initialValue: T): MyArray<T>;
+}
+const myArray: MyArray<number> = [1, 2, 3, 4, 5];
+let a = myArray.reduce((a, b) => a + b, 0);
+let b = myArray.reduce2((a, b) => a + b, 0);
